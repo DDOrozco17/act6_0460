eca93fb227d7d87f908db3dc143babd06e1610c0
DIEGO_DIAZ_OROZCO <a.21308051280460@cbtis128.edu.mx>
2024-05-02 18:40:03 +0000

    act

---
+++ lib/data/product_data.dart
@@ -0,0 +1,16 @@
+import 'package:act6_0460/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/LapizRojo.png",
+      title: "Lapiz Rojo",
+      description: "Categoria: Escolaridad \n Precio: 455 \n No.: 2 "),
+  ProductModel(
+      img: "assets/images/pegamentohome.png",
+      title: "Pegamento",
+      description: "Categoria: Oficina Precio: 235 Tamaño: Basico"),
+  ProductModel(
+      img: "assets/images/tapehome.png",
+      title: "Tape",
+      description: "Categoria: Oficina Precio: 125 Medida(metros): 100"),
+];
